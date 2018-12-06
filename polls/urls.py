9b9d37d509b31cd6199fbd64518d7745d6e0ad7a
Talitha <tapumar@gmail.com>
2018-12-06 16:21:24 -0200

    lista de polls

--- polls/urls.py
+++ polls/urls.py
@@ -5,21 +5,25 @@ from .apiviews import ChoiceList, CreateVote, LoginView, PollViewSet, UserCreate
 from rest_framework_swagger.views import get_swagger_view
 from .views import PollList, PollDetail
 
+from .apps import PollsConfig
+
+app_name = PollsConfig.name
+
 schema_view = get_swagger_view(title='Polls API')
 
 router = DefaultRouter()
-router.register('polls', PollViewSet, base_name='polls')
+router.register('api/polls', PollViewSet, base_name='polls')
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
-    path("users/", UserCreate.as_view(), name="user_create"),
-    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(),
+    path("api/polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("api/users/", UserCreate.as_view(), name="user_create"),
+    path("api/polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(),
         name="create_vote"),
     path(r'swagger-docs/', schema_view),
     path(r'docs/', include_docs_urls(title='Polls API')),
-    path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path(r'polls/list/', PollList.as_view(), name="polls_list"),
+    path(r'polls/<int:pk>/', PollDetail.as_view(), name="polls_detail"),
 ]
 
 urlpatterns += router.urls
--- polls/views.py
+++ polls/views.py
@@ -22,30 +22,33 @@ class PollDetail(APIView):
     def get(self, request, pk):
         poll = get_object_or_404(Poll, pk=pk)
         choices = Choice.objects.filter(poll=poll)
+        # for choice in choices:
+        #     votes = Vote.objects.filter(poll=poll,choice=choice).count()
+        #     porc += votes
         serializer = PollSerializer(poll)
         return Response({'serializer': serializer, 'poll': poll, 'choices':choices})
 
-    def post(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        serializer = PollSerializer(poll, data=request.data)
-        if not serializer.is_valid():
-            return Response({'serializer': serializer, 'poll': poll})
-        serializer.save()
-        return redirect('polls-list')
+    # def post(self, request, pk):
+    #     poll = get_object_or_404(Poll, pk=pk)
+    #     serializer = PollSerializer(poll, data=request.data)
+    #     if not serializer.is_valid():
+    #         return Response({'serializer': serializer, 'poll': poll})
+    #     serializer.save()
+    #     return redirect('polls-list')
 
 class PollList(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'polls/poll_list.html'
+    template_name = 'polls/polls_list.html'
 
-    def get(self, request, pk):
-        poll = Poll.objects.get().all()
-        serializer = PollSerializer(poll)
-        return Response({'serializer': serializer, 'poll': poll})
+    def get(self, request):
+        polls = Poll.objects.filter().all()
+        serializer = PollSerializer(polls)
+        return Response({'serializer': serializer, 'polls': polls})
 
-    def post(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        serializer = PollSerializer(poll, data=request.data)
-        if not serializer.is_valid():
-            return Response({'serializer': serializer, 'poll': poll})
-        serializer.save()
-        return redirect('polls-list')
+    # def post(self, request, pk):
+    #     poll = get_object_or_404(Poll, pk=pk)
+    #     serializer = PollSerializer(poll, data=request.data)
+    #     if not serializer.is_valid():
+    #         return Response({'serializer': serializer, 'poll': poll})
+    #     serializer.save()
+    #     return redirect('polls-list')
